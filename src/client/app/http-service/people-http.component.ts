34da9d3dea48399078e4148382e43229cb745ab0
Rashied Imambaks <r@imambaks.nl>
2016-01-28 17:02:36 +0100

    JSON file

--- src/client/app/http-service/people-http.component.ts
+++ src/client/app/http-service/people-http.component.ts
@@ -23,20 +23,49 @@ export class PeopleComponent implements OnInit {
         this.http.get('app/http-service/eng2-2013-14.json')
             .map(res=> res.json())
             .subscribe(res => {
+                console.log('INITIAL')
+                console.log(res);
 
 
                 let dataObject = d3.merge(
                     res.map(
-                        (d) => { d.Games.forEach(
-                                (g) => { g.Date = d.Date }
+                        (d) => {
+                            d.Games.forEach(
+                                (g) => {
+                                    g.Date = d.Date
+                                }
                             );
 
                             return d.Games
                         })
-                )
+                );
 
+                let dataMap = d3.map();
+
+                let xObject = d3.merge([
+                    d3.nest().key(d => d.Away).entries(dataObject),
+                    d3.nest().key(d => d.Home).entries(dataObject)
+                ]).forEach(d => {
+
+                    if (dataMap.has(d.key)) {
+
+                        dataMap.set(d.key, d3.merge([dataMap.get(d.key), d.values]))
+                            .sort((a, b) => d3.ascending(a.Date, b.Date))
+
+                    } else {
+                         dataMap.set(d.key, d.values);
+
+                    }
+
+
+                });
+
+
+                console.log('Secondary dataSTRUCT');
                 console.log(dataObject);
 
+                console.log('Third Datastructure');
+                console.log(dataMap);
 
             })
 
