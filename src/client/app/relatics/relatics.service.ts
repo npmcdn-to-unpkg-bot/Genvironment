38a07b910af46db027c202c9db5f5dc31d10e289
Rashied Imambaks <r@imambaks.nl>
2016-03-06 10:15:03 +0100

    changed some things

--- src/client/app/relatics/relatics.service.ts
+++ src/client/app/relatics/relatics.service.ts
@@ -9,39 +9,36 @@ export class RelaticsService {
     GetData(operationName:string, workspaceId:string, entryCode:string, objectId:string):Promise<HTMLDocument> {
 
         // xml data for the SOAP request
-        let xml = '<soap:Envelope ' + 'xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" ' +
-            'xmlns:xsd="http://www.w3.org/2001/XMLSchema" ' +
-            'xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">' +
-            '<soap:Body>' +
-            '<GetResult xmlns="http://www.relatics.com/">' +
-            '<Operation>' + operationName + '</Operation>' +
-            '<Identification>' +
-            '<Identification>' +
-            '<Workspace>' + workspaceId + '</Workspace>' +
-            '</Identification>' +
-            '</Identification>' +
-            '<Parameters>' +
-            '<Parameters>' +
-            '<Parameter Name="parameter" ' +
-            'Value="' +
-            objectId
-            + '"/>' +
-            '</Parameters>' +
-            '</Parameters>' +
-            '<Authentication>' +
-            '<Authentication>' +
-            '<Entrycode>' + entryCode + '</Entrycode>' +
-            '</Authentication>' +
-            '</Authentication>' +
-            '</GetResult>' +
-            '</soap:Body>' +
-            '</soap:Envelope>';
+        let xml = `<soap:Envelope   xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"  
+            xmlns:xsd="http://www.w3.org/2001/XMLSchema"  
+            xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/"> 
+            <soap:Body> 
+            <GetResult xmlns="http://www.relatics.com/"> 
+            <Operation>${operationName}</Operation> 
+            <Identification> 
+            <Identification> 
+            <Workspace>${workspaceId}</Workspace> 
+            </Identification> 
+            </Identification> 
+            <Parameters> 
+            <Parameters> 
+            <Parameter Name="parameter"  
+            Value="${objectId}"/> 
+            </Parameters> 
+            </Parameters> 
+            <Authentication> 
+            <Authentication> 
+            <Entrycode>${entryCode}</Entrycode> 
+            </Authentication> 
+            </Authentication> 
+            </GetResult> 
+            </soap:Body> 
+            </soap:Envelope>`;
 
 
         // Define a SOAP request for relatics endpoint
-        let SoapRequest;
+        let SoapRequest= new XMLHttpRequest();
         let url = 'https://arcadis.relaticsonline.com/DataExchange.asmx?wsdl';
-        SoapRequest = new XMLHttpRequest();
 
         // Send xml data to endpoint
         SoapRequest.open('POST', url, false);
