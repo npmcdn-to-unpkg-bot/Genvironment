00f85d4330a8af326311b56d019dfcaab787edf2
Rashied Imambaks <r@imambaks.nl>
2016-03-27 12:33:13 +0200

    gulp injext added

--- src/client/app/course-1/course-1.component.ts
+++ src/client/app/course-1/course-1.component.ts
@@ -3,14 +3,25 @@ import {Component, Input} from 'angular2/core'
 
 @Component({
     selector: 'course-1',
-    template: `<h1>Hello bitiah van mij</h1>`
+    template: `<h1>Hello bitiah van mij</h1>
+
+        <ul>
+        <li *ngFor="#item of items">
+        
+        {{item}}
+</li>
+        </ul>
+`
 })
 
 
 export class CourseOneComponent {
 
-    @Input()
-    CourseOneData
+    items:string[];
+
+    constructor() {
+        this.items = ['as', 'as']
+    }
 
 
 }
