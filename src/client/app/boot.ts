e9dfd8e2a086f5914aaa57d149237386073d5713
Rashied Imambaks <r@imambaks.nl>
2016-04-30 09:16:04 +0200

    typscript compiles fixes

--- src/client/app/boot.ts
+++ src/client/app/boot.ts
@@ -1,5 +1,5 @@
 /// <reference path="../../../node_modules/angular2/typings/browser.d.ts" />
-/// <reference path="../../../typings/d3.d.ts" />
+/// <reference path="../../../typings/tsd.d.ts" />
 
 import {bootstrap} from "angular2/platform/browser";
 import {AppComponent} from "./app.component";
